68f7cedac02e9fea44b37286a6eff92273bafb89
jjambbongg <jjambbongg@gmail.com>
2017-01-24 09:46:47 +1100

    Spring MVC Created

---
+++ SpringMVC/src/main/java/com/javalec/spring_ex_pjt/SampleRequestMapping.java
@@ -0,0 +1,14 @@
+package com.javalec.spring_ex_pjt;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/board")
+public class SampleRequestMapping {
+
+	@RequestMapping("/view")
+	public String view() {
+		return "/board/view";
+	}
+}
