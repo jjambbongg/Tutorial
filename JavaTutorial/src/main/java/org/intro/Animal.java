e7f56a5f81f82f15f4c46f21bb049fef66ec6566
jjambbongg <jjambbongg@gmail.com>
2017-01-24 09:45:07 +1100

    Java Tutorial Created

---
+++ JavaTutorial/src/main/java/org/intro/Animal.java
@@ -0,0 +1,17 @@
+package org.intro;
+
+
+public enum Animal {
+	
+	CAT("Meow", "Kitty"), DOG("Bark", "Doggy"), BIRD("Jingle","Birdy");
+	
+	String str1 = "";
+	String str2 = "";
+	
+	Animal(String str1, String str2) {
+		this.str1 = str1;
+		this.str2 = str2;
+	}
+	
+	
+}
